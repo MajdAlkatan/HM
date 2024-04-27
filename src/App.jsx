8c33fbf9651c1f21c1fa53da3dbf6e21a14dd430
MajdAlkatan <majdalkatan989@gmail.com>
2024-04-27 19:57:29 +0300

    HM

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 import Login from './Login/Login'
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom';
+import videoFile from './assets/vid.mp4';
 
 function App() {
- 
+
 
   return (
-    <Router>
+    <div className='vid' >  
+     <video autoPlay loop muted>
+        <source src={videoFile} type="video/mp4" />
+      </video> 
+      <Router>
       <Login />
     </Router>
+    </div>
   )
 }
 
