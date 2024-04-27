124b848bec95523e3f88d34487d83306b9b201a9
hazemabuzaid <hazemabuzaid16@gmail.com>
2024-04-27 20:03:29 +0300

    HM

--- src/Login/LoginSlice.js
+++ src/Login/LoginSlice.js
@@ -1,10 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
 
+
+export const login = createAsyncThunk(
+    'users/login',
+    async({ email, password }, thunkAPI) => {
+        const body = JSON.stringify({
+            email,
+            password,
+        });
+
+        try {
+            const res = await axios('/auth/login/web/', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body,
+            });
+
+            const data = await res.json();
+
+            if (res.status === 200) {
+                return data;
+            } else {
+                return thunkAPI.rejectWithValue(data);
+            }
+        } catch (err) {
+            return thunkAPI.rejectWithValue(err.response.data);
+        }
+    }
+);
 const loginSlice = createSlice({
         name: 'login',
         initialState: {},
         reducers: {
 
+        },
+        extraReducers: builder => {
+            builder
+                .addCase(login.pending, state => {
+                    state.loading = true;
+                })
+                .addCase(login.fulfilled, state => {
+                    state.loading = false;
+                    state.isAuthenticated = true;
+                })
+                .addCase(login.rejected, state => {
+                    state.loading = false;
+                })
         }
     })
     //export const {} = loginSlice.actions;
