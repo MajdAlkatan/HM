471703be99dfb466917baec960c72e9fe4c482e8
MajdAlkatan <majdalkatan989@gmail.com>
2024-04-29 10:52:23 +0300

    Initial Commit

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,10 +1,32 @@
 import './Login.css'
+import  { useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/fontawesome-free-solid'; // Import the specific icon
+import { faUser,faLock } from '@fortawesome/fontawesome-free-solid'; // Import the specific icon
 import videoFile from '../assets/vid.mp4';
-//import { useSelector,useDispatch} from "react-redux"
+import { useSelector,useDispatch} from "react-redux"
 import { Link } from 'react-router-dom'
+import { login } from './LoginSlice';
+// import { useNavigate } from 'react-router-dom'; 
 const Login = () => {
+  const dispatch = useDispatch();
+  const loginState = useSelector(state => state.login); // Assuming your login state is stored under 'login'
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const navigate = useNavigate();
+ 
+  const handleLogin =async (e) => {
+     e.preventDefault();
+     try {
+      await dispatch(login({ email, password })).unwrap();
+      // Now that the login action has completed, check the login state and navigate
+      if (loginState) {
+        console.log('success');
+        // navigate('/home'); // Navigate to the home page
+      }
+   } catch (error) {
+      console.error('Login failed:', error);}
+  };
+ 
   return (
     <div className='container'>
 <div className='image'>
@@ -18,13 +40,15 @@ const Login = () => {
     <div className='login'>
       <span className='welcom'>Welcom Back...</span>
         <div className='inputs'>
-          <input className='user' type="text" placeholder='user name'/>
+          <input className='user' type="text" placeholder='user name'      value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
           <FontAwesomeIcon className='icon-user' icon={faUser}/>
-          <input className ='password' type="text" placeholder='password' />
-          <FontAwesomeIcon className='icon-password' icon="fa-solid fa-lock" />
+          <input className ='password' type="text" placeholder='password'   value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
+          <FontAwesomeIcon className='icon-password' icon={faLock} />
           <Link href="/" className='forget-password'>forget your password?</Link>        
           </div>
-        <button className='login-button'>Login</button>
+        <button className='login-button' onClick={handleLogin}>Login</button>
       </div>
     </div> 
   )
