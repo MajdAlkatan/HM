e352d983bf87f1fad6c3e0dc4d6c1af194f7b021
MajdAlkatan <majdalkatan989@gmail.com>
2024-06-03 01:39:03 +0300

    Majd_king_OF_All_Generations

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
-// Portfolio.jsx
 import PropTypes from "prop-types";
+import Slider from "react-slick";
 import './Portfolio.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Portfolio({ images }) {
   // Check if images is an array
@@ -9,9 +11,36 @@ function Portfolio({ images }) {
     return null; // Or render a fallback UI
   }
 
+  // Slider settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div className="portfolio">
-      <div className="imgs-container">
+      <Slider {...settings} className="imgs-container">
         {images.map((image, index) => (
           <div key={index} className="box" onClick={() => window.location.href = `/hotel-Page`}> {/* Adjust navigation as needed */}
             <img src={image} alt={`Image ${index}`} />
@@ -21,7 +50,7 @@ function Portfolio({ images }) {
             </div>
           </div>
         ))}
-      </div>
+      </Slider>
       <a href="#cd" className="more">More</a>
     </div>
   );
